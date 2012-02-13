e41043c7d4fee7dca24d7ff563ced8c75c474ce3
Vincent Yang <yusyang686@gmail.com>
2012-02-12 20:59:29 -0500

    got authentication method working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
+require 'digest'
 class User < ActiveRecord::Base
-  attr_accessor :password
+  attr_accessor   :password
   attr_accessible :name, :email, :password, :password_confirmation
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -15,13 +16,36 @@ class User < ActiveRecord::Base
                         
   before_save :encrypt_password
   
+  def has_password?(submitted_password)
+    # Compare encrypted_password with the encrypted version of 
+    # submitted_password.
+    encrypted_password == encrypt(submitted_password)
+  end
+  
+  def self.authenticate(email, submitted_password)
+    user = self.find_by_email(email)
+    #why these following 2 lines can't be swapped?
+    return nil if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+  
   private
   
   def encrypt_password
-    self.encryped_password = encrypt(self.password)
+    self.salt = make_salt if new_record?
+    self.encrypted_password = encrypt(self.password) 
+    # "self.password" is the same as "password" here, same as the following
   end
   
   def encrypt(str)
-    str
+    secured_hash("#{salt}--#{str}")
+  end
+  
+  def make_salt
+     secured_hash("#{Time.now.utc}--#{self.password}")
+  end
+  
+  def secured_hash(str)
+    Digest::SHA2.hexdigest(str)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,11 +9,12 @@ describe User do
       :password => "foobar",
       :password_confirmation => "foobar"
     }
+    @user = User.create!(@attr) #for password part
   end
   
   
   it "should be able to create a user with valid attr" do
-    User.create!(@attr)
+    @user.should be_valid
   end
   
   it "should require a name" do
@@ -21,7 +22,7 @@ describe User do
     should_not be_valid
   end
   
-  it "should have an name that with proper length" do
+  it "should have a name that with proper length" do
     long_name = "a" * 51
     long_name_user = User.new(@attr.merge(:name => long_name)).
     should_not be_valid
@@ -42,41 +43,86 @@ describe User do
   end
   
   it "should have an unique email" do
-    User.create!(@attr)
-    User.new(@attr.merge(:name => "different_names_same_email")).
+    # testing upcase email address
+    User.new(@attr.merge(:email => @attr[:email].upcase)).
     should_not be_valid
   end
   
-  it "should require a password" do
-    User.new(@attr.merge(:password => '', :password_confirmation => '')).
-    should_not be_valid
-    # this one is not validating properly, but passed…
-  end
-  
-  it "should reqire a matching pw and pw_confirmation set" do
-    User.new(@attr.merge(:password_confirmation => '')).
-    should_not be_valid
-  end
-  
-  it "should check the length of the password" do
-    invalid_passwords = [] << 'a' * 5 << 'a' * 61
-    invalid_passwords.each do |i|
-      User.new(@attr.merge(:password => "i", :password_confirmation => "i")).
-      should_not be_valid
-    end
-  end
-
-  describe "password encryption" do
-    before(:each) do
-      @user = User.create!(@attr)
+  describe "password" do
+    it "should respond to password" do
+      User.new(@attr).should respond_to(:password)
     end
     
-    it "should respond to the encryped_password method" do
+    it "should respond to password confirmation as well" do
+      User.new(@attr).should respond_to(:password_confirmation)
+    end
+    
+    it "should require a password" do
+      User.new(@attr.merge(:password => '', :password_confirmation => '')).
+      should_not be_valid
+      # this one is not validating properly, but passed…
+    end
+  
+    it "should require a matching pw and pw_confirmation set" do
+      User.new(@attr.merge(:password_confirmation => '')).
+      should_not be_valid
+    end
+  
+    it "should check the length of the password" do
+      invalid_passwords = [] << 'a' * 5 << 'a' * 61
+      invalid_passwords.each do |i|
+        User.new(@attr.merge(:password => "i", :password_confirmation => "i")).
+        should_not be_valid
+      end
+    end
+  end
+  
+  describe "password encryption" do
+    
+    it "should respond to the encryped_password" do
       @user.should respond_to(:encrypted_password)
     end
     
     it "should be able to set the encryped password" do
       @user.encrypted_password.should_not be_blank
+    end
+    
+    it "should have a salt" do
+      @user.should respond_to(:salt)
+    end
+
+    describe "has_password?" do
+      
+      it "should respond to has_password" do
+        @user.should respond_to(:has_password?)
+      end
+      
+      it "should should be true if the passwords match" do
+        @user.has_password?(@attr[:password]).should be_true
+      end
+
+      it "should be false when passwords don't match" do
+        @user.has_password?("invalid_password").should be_false
+      end
+    end
+  end
+  
+  describe "authenticate method" do
+    
+    it "should exist" do
+      User.should respond_to(:authenticate)
+    end
+    
+    it "should return nil on email/pw mismatch" do
+      User.authenticate(@attr[:email], "wrongpassword").should be_nil
+    end
+    
+    it "should return nil for no email/no user" do
+      User.authenticate('not_exist@email.com', @attr[:password]).should be_nil
+    end
+    
+    it "should return a user on match" do
+      User.authenticate(@attr[:email], @attr[:password]).should == @user
     end
   end
 end
